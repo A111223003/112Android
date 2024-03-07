d6a9718e7f66503dfe760a43fc9454e2727bfff8
A111223003 <A111223003@mail.shu.edu.tw>
2024-03-07 17:06:14 +0800

    0307

---
+++ app/src/main/java/com/example/counter1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.counter1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void button(View view){
+        int counter;
+        TextView value = (TextView) findViewById(R.id.textView);
+        counter = Integer.parseInt(value.getText().toString());
+        counter++;
+        value.setText(Integer.toString(counter));
+    }
+
+    public void button_click(View view){
+        TextView value=(TextView) findViewById(R.id.textView);
+        value.setText(R.string.initial_value);
+    }
+}
